7e16ffb09246de4c52c6515388c646ae6257e3ab
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-11-02 18:16:00 -0700

    Finished Part3, models. Migrated new updates

--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -6,7 +6,7 @@ import uuid # Required for unique book instances
 class Genre(models.Model):
     """Model representing a book genre"""
 
-    name = models.CharFiedl(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
+    name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Science Fiction)')
 
     def __str__(self):
         """String for representing the Model object"""
@@ -69,3 +69,24 @@ class BookInstance(models.Model):
         """String for representing the Model object"""
         
         return f'{self.id} ({self.book.title})'
+
+class Author(models.Model):
+    """Model representing an author"""
+
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def get_absolute_url(self):
+        """Returns the url to access a particular author instance"""
+        
+        return reverse('author-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object"""
+
+        return f'{self.last_name}, {self.first_name}'
